9edd52f0456ab1146adbfa929527e1b260de39a8
David Nintang <nintang48@gmail.com>
2024-12-25 00:48:09 -0600

    Add ESLint configuration to ignore errors during production builds in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // your config options
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  }
 }
 
 module.exports = nextConfig 
